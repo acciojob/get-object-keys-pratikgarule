6a0151ef2bff384c9786d0407ea1a9133d799f63
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-06-24 07:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+Object.prototype.getKeys = function(){
+    return Object.keys(this);
+}
+
+let student = {
+    name: "John Doe",
+    // age: 20,
+    // grade: 10,
+}
